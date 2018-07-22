888eae98d97b5c8234838f57791a15eefd265ecb
Mike Roberts <mike@kik.com>
2018-07-22 19:23:12 -0400

    Setup dummy notification view model

--- demo/src/main/java/com/github/mproberts/rxdatabindingdemo/home/vm/NotificationViewModel.java
+++ demo/src/main/java/com/github/mproberts/rxdatabindingdemo/home/vm/NotificationViewModel.java
@@ -1,4 +1,28 @@
 package com.github.mproberts.rxdatabindingdemo.home.vm;
 
-class NotificationViewModel {
+import io.reactivex.Flowable;
+
+public class NotificationViewModel {
+
+    private final int _notificationId;
+    private final String _title;
+    private final String _subtitle;
+
+    public NotificationViewModel(int notificationId, String title, String subtitle) {
+        _notificationId = notificationId;
+        _title = title;
+        _subtitle = subtitle;
+    }
+
+    public int notificationId() {
+        return _notificationId;
+    }
+
+    public Flowable<String> title() {
+        return Flowable.just(_title);
+    }
+
+    public Flowable<String> subtitle() {
+        return Flowable.just(_subtitle);
+    }
 }
