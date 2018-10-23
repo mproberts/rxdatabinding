81545efdd93a4f6cff208908e8e80fa62f99520f
Mike Roberts <mike@kik.com>
2018-10-22 20:42:40 -0400

    Extract enum level value adapter

--- library/src/main/java/com/github/mproberts/rxdatabinding/bindings/LevelValueAdapter.java
+++ library/src/main/java/com/github/mproberts/rxdatabinding/bindings/LevelValueAdapter.java
@@ -1,25 +1,6 @@
 package com.github.mproberts.rxdatabinding.bindings;
 
-import java.util.HashMap;
-import java.util.Map;
-
-interface LevelValueAdapter {
+public interface LevelValueAdapter {
 
     int convertValue(Object value);
-}
-
-class EnumLevelValueAdapter<T> implements LevelValueAdapter {
-
-    Map<T, Integer> _enumValues = new HashMap<>();
-
-    @Override
-    public int convertValue(Object value) {
-        return _enumValues.get((T) value);
-    }
-
-    public EnumLevelValueAdapter<T> map(T enumValue, int level) {
-        _enumValues.put(enumValue, level);
-
-        return this;
-    }
 }
